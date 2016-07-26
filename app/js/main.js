7433c1768af120abec6bfc690ccdd1dfafe5df4c
AmmarSaadDine <ammar.as.89@gmail.com>
2016-07-26 18:08:24 +0200

    logging

--- app/js/main.js
+++ app/js/main.js
@@ -30,8 +30,7 @@ function subscribe() {
   reg.pushManager.subscribe({userVisibleOnly: true}).
   then(function(pushSubscription){
     sub = pushSubscription;
-    console.log('length of the url:', sub.endpoint.length);
-    console.log('Subscribed! Endpoint:' , sub.endpoint);
+    console.log('Subscribed! Endpoint:' , sub.endpoint.toString());
     subscribeButton.textContent = 'Unsubscribe';
     isSubscribed = true;
   });
