cfbe0ada873e62246ce15dcdce4b351cb0acf2aa
AmmarSaadDine <ammar.as.89@gmail.com>
2016-07-26 17:40:26 +0200

    remove the alert from main.js

--- app/js/main.js
+++ app/js/main.js
@@ -31,7 +31,6 @@ function subscribe() {
   then(function(pushSubscription){
     sub = pushSubscription;
     console.log('Subscribed! Endpoint:', sub.endpoint);
-    alert(sub.endpoint);
     subscribeButton.textContent = 'Unsubscribe';
     isSubscribed = true;
   });
